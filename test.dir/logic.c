dd94d3a68bb459107326d50ca50a0b667da76446
Pan Seow Hwee <a0130622@u.nus.edu>
2016-09-29 16:03:28 +0800

    Empty code files

---
+++ test.dir/logic.c
@@ -0,0 +1,36 @@
+// logic.c
+// AutoMod 12.6.1 Generated File
+// Build: 12.6.1.12
+// Model name:	TEST
+// Model path:	D:\SEM5 mods\IE3110 Simulation\TEST.dir\
+// Generated:	Thu Sep 29 16:01:23 2016
+// Applied/AutoMod Licensee Confidential
+// NO DISTRIBUTION OR REPRODUCTION RIGHTS GRANTED!
+// Copyright (c) 1988-2015 Applied Materials All rights reserved.
+//
+// All Rights Reserved.  Reproduction or transmission in whole or
+// in part, in any form or by any means, electronic, mechanical or
+// otherwise, is prohibited without the prior written consent of
+// copyright owner.
+//
+// Licensed Material - Property of Applied Materials, Inc.
+//
+// Applied Materials, Inc.
+// 3050 Bowers Drive
+// P.O. Box 58039
+// Santa Clara, CA 95054-3299
+// U.S.A.
+//
+
+
+#include "cdecls.h"
+
+
+
+
+/* init function for logic.m */
+void
+model_logic_init(struct model_struct* data)
+{
+}
+
